45684bc7c3058724e9f52357d5ace6fd806d45e3
Roberto Sobachi <roberto.sobachi@me.com>
2017-10-13 08:24:43 +0100

    Showing the moves step, in progress...

--- src/Game.js
+++ src/Game.js
@@ -53,6 +53,17 @@ class Game extends React.Component {
     const current = history[history.length - 1];
     const winner = this.calculateWinner(current.squares);
 
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
@@ -70,6 +81,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
+          <ol>{moves}</ol>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
